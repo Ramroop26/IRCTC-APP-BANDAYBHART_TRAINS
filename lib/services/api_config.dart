8745786129683ec42a1a88ee11d4e19d83ef24b3
Ramroop26 <ramroop26io071@satiengg.in>
2026-07-10 00:19:56 +0530

    chore: change api baseUrl to custom domain www.bandaybhart.com

--- lib/services/api_config.dart
+++ lib/services/api_config.dart
@@ -1,7 +1,3 @@
-import 'package:flutter/foundation.dart';
-
 class ApiConfig {
-  // Use relative path for web so that it dynamically fits the host,
-  // and fallback to localhost:3000 for mobile/desktop/emulator.
-  static const String baseUrl = kIsWeb ? '' : 'http://localhost:3000';
+  static const String baseUrl = 'https://www.bandaybhart.com';
 }
